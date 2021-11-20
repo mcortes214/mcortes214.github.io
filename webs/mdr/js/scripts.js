1f1e12800a6bf676e495b237a3f3a63924d9b003
Rizoma <rizomadg@gmail.com>
2021-11-20 08:28:00 -0300

    20 NOV 0827

--- webs/mdr/js/scripts.js
+++ webs/mdr/js/scripts.js
@@ -20,10 +20,24 @@ $(document).ready(function(){
 
   //Panzoom
   const element = document.getElementById('panzoom')
-  const panzoom = Panzoom(element, {
-    disableZoom: true,
+  if(element){
+    const panzoom = Panzoom(element, {
+      disableZoom: true,
+    });
+  }
+
+
+  //Coleccion pandemica - gifs
+
+  $('.gif-coleccion-pandemica').on('mouseenter',function(){
+    let previousImageUrl = $(this).attr('src');
+    console.log(previousImageUrl);
+    let urlgif = $(this).data('url-gif');
+    console.log(urlgif);
+    $(this).attr('src',urlgif);
+    let gifTimeout = window.setTimeout(function(){
+      $(this).attr('src',previousImageUrl);
+    }, 1000);
   });
-
-
 
 });//Fin documentReady
