74fa52c96baff76dac5a0deabc83d82625f2ffd7
Maxi <mcortes.na@gmail.com>
2022-01-21 12:40:51 -0300

    Silvia Rubinson primera presentación

--- webs/silvia-rubinson/js/scripts.js
+++ webs/silvia-rubinson/js/scripts.js
@@ -3,28 +3,35 @@ $('.fading-fullscreen-slider').slick({
     'dots': false,
     'fade': true,
     'autoplay': true,
-    'autoplaySpeed': 2000,
+    'autoplaySpeed': 5000,
     'pauseOnFocus': false,
     'pauseOnHover': false,
 });
 
-  //Panzoom
-  let panzooms = document.querySelectorAll('.js-pan-zoom');
-    for(let element of panzooms){
-        if(element){
-            console.log(element);
-            const panzoom = Panzoom(element, {
-              animate:true,
-              duration: 200,
-              easing: 'ease-in-out',
-              maxScale: 1,
-              minScale: 0.125,
-              startScale: 0.5,
-              startX: -1,
+//height equalizing rows
+
+Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
+    console.log('images finished loading');
+    resizeImages();
+});
 
 
-            });
-            const parent = element.parentElement;
-            parent.addEventListener('wheel', panzoom.zoomWithWheel);
+function resizeImages(){
+    const heightEqualizingRows = document.querySelectorAll('.cft-js-height-equalizing-row');
+
+    for (let row of heightEqualizingRows){
+        const images = row.querySelectorAll('img');
+        let sumOfWidths = 0;
+        for(let image of images){
+                image.style.height = '1000px';
+                sumOfWidths += image.clientWidth;
+        }
+        let paddings = 20*(images.length - 1);
+        console.log(paddings);
+        let shrinkingRatio = (row.clientWidth - paddings) / sumOfWidths;
+        for(let image of images){
+            image.style.height = image.clientHeight * shrinkingRatio + 'px';
+            //El ancho se reescala solo
         }
     }
+}
