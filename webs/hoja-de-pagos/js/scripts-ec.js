5c17ddaa6ec958f2339d7941579164255cf7fca7
Maxi <mcortes.na@gmail.com>
2022-02-15 10:15:27 -0300

    hoja de pagos Provalores Ecuador

---
+++ webs/hoja-de-pagos/js/scripts-ec.js
@@ -0,0 +1,51 @@
+console.log('holi');
+
+$(document).ready(function(){
+
+  $('.imprimir').click(function(){
+      window.print();
+  });
+
+  $('#crear-hoja').click(function(){
+    crearFechas();
+    $('.overlay-pagina').css({'left' : '-100vw'});
+    $('.overlay-pagina').css({'opacity' : '0'});
+  });
+
+});
+
+function crearFechas() {
+  var d = $('#input-fecha').val();
+  if(d){
+    moment.locale('es');
+    var fechaDeHoy = moment(d);
+    var fechaHoyFormato = fechaDeHoy.format('LL');
+    console.log(fechaHoyFormato);
+
+    //Fecha inicial
+    $('.fecha-de-hoy').append(fechaHoyFormato);
+
+    //Fechas subsiguientes
+    var semana;
+
+    for (var i = 0; i < 24; i++) {
+       semana = fechaDeHoy.add(15, 'days');
+       $('.bloque-1').append('<p class="fecha-pago">⬜ '+semana.format('LL')+'</p>');
+    }
+
+    for (var i = 0; i < 12; i++) {
+       semana = fechaDeHoy.add(15, 'days');
+       $('.bloque-2').append('<p class="fecha-pago">⬜ '+semana.format('LL')+'</p>');
+    }
+
+    for (var i = 0; i < 12; i++) {
+       semana = fechaDeHoy.add(15, 'days');
+       $('.bloque-3').append('<p class="fecha-pago">⬜ '+semana.format('LL')+'</p>');
+    }
+
+    for (var i = 0; i < 12; i++) {
+       semana = fechaDeHoy.add(15, 'days');
+       $('.bloque-4').append('<p class="fecha-pago">⬜ '+semana.format('LL')+'</p>');
+    }
+  }
+}
